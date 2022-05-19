bc788065865f3e0cb29a8fdc73f6d66d8b466eba
chickensalt <nsdj.sharma@gmail.com>
2022-05-19 19:46:59 +1000

    remove unnecessary imports

--- src/main/java/frc/robot/subsystems/NavigationSub.java
+++ src/main/java/frc/robot/subsystems/NavigationSub.java
@@ -4,12 +4,8 @@ import frc.robot.Constants;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
-// import edu.wpi.first.math.geometry.Pose2d;
-// import edu.wpi.first.math.geometry.Rotation2d;
-// import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.ADIS16470_IMU;
 import edu.wpi.first.wpilibj.Encoder;
-// import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
