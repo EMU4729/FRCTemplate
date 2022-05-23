7bac225d8397efcab488992f5c4043915fcffe48
chickensalt <nsdj.sharma@gmail.com>
2022-05-23 18:11:37 +1000

    remove unused import

--- src/main/java/frc/robot/subsystems/DriveSub.java
+++ src/main/java/frc/robot/subsystems/DriveSub.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Variables;
 import frc.robot.utils.Clamper;
 
 /**
