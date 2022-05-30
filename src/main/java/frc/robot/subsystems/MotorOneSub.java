2a436c4072632ba6eaf0fc41fec96ae0d30503bd
chickensalt <nsdj.sharma@gmail.com>
2022-05-30 18:20:24 +1000

    add basic logging

--- src/main/java/frc/robot/subsystems/MotorOneSub.java
+++ src/main/java/frc/robot/subsystems/MotorOneSub.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.utils.Clamper;
+import frc.robot.utils.logger.Logger;
 
 public class MotorOneSub extends SubsystemBase {
   private final Constants constants = Constants.getInstance();
@@ -17,6 +18,7 @@ public class MotorOneSub extends SubsystemBase {
   public void setMotorSpeed(double speed) {
     speed = Clamper.absUnit(speed);
     motor.set(speed);
+    Logger.info("MotorOneSub : Speed = " + speed);
   }
 
 }
--- src/main/java/frc/robot/subsystems/MotorTwoSub.java
+++ src/main/java/frc/robot/subsystems/MotorTwoSub.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.utils.Clamper;
+import frc.robot.utils.logger.Logger;
 
 public class MotorTwoSub extends SubsystemBase {
   private final Constants constants = Constants.getInstance();
@@ -17,5 +18,6 @@ public class MotorTwoSub extends SubsystemBase {
   public void setMotorSpeed(double speed) {
     speed = Clamper.absUnit(speed);
     motor.set(speed);
+    Logger.info("MotorTwoSub : Speed = " + speed);
   }
 }
