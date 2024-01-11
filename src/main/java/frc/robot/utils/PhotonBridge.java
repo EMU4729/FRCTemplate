77e89e872f34aa3de40e40374de6c31e4f28dff4
Neel Sharma <nsdj.sharma@gmail.com>
2024-01-11 18:42:23 +1100

    partial vision code

--- src/main/java/frc/robot/utils/PhotonBridge.java
+++ src/main/java/frc/robot/utils/PhotonBridge.java
@@ -9,11 +9,13 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.simulation.PhotonCameraSim;
+import org.photonvision.simulation.SimCameraProperties;
 import org.photonvision.simulation.VisionSystemSim;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
@@ -29,13 +31,14 @@ public class PhotonBridge {
 
   // Simulation
   private VisionSystemSim visionSim;
+  private SimCameraProperties camProps;
   private PhotonCameraSim camSim;
 
   public PhotonBridge() {
     AprilTagFieldLayout tempFieldLayout;
 
     try {
-      tempFieldLayout = new AprilTagFieldLayout(AprilTagFields.k2023ChargedUp.m_resourceFile);
+      tempFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
     } catch (IOException e) {
       tempFieldLayout = new AprilTagFieldLayout(List.of(), 0, 0);
       Logger.error("PhotonSub : Error reading AprilTag field layout: " + e);
@@ -48,6 +51,38 @@ public class PhotonBridge {
     if (RobotBase.isSimulation()) {
       visionSim = new VisionSystemSim(Constants.vision.PHOTON_CAMERA_NAME);
       visionSim.addAprilTags(fieldLayout);
+
+
+      camProps = new SimCameraProperties();
+      // A 640 x 480 camera with a 100 degree diagonal FOV.
+      camProps.setCalibration(640, 480, Rotation2d.fromDegrees(100));
+      // Approximate detection noise with average and standard deviation error in
+      // pixels.
+      camProps.setCalibError(0.25, 0.08);
+      // Set the camera image capture framerate (Note: this is limited by robot loop
+      // rate).
+      camProps.setFPS(20);
+      // The average and standard deviation in milliseconds of image data latency.
+      camProps.setAvgLatencyMs(35);
+      camProps.setLatencyStdDevMs(5);
+
+      // TODO: Find out why instantiating PhotonCameraSim crashes the robot
+      // camSim = new PhotonCameraSim(cam, camProps);
+      // camSim.enableProcessedStream(true);
+
+      // Our camera is mounted 0.1 meters forward and 0.5 meters up from the robot
+      // pose,
+      // (Robot pose is considered the center of rotation at the floor level, or Z =
+      // 0)
+      Translation3d robotToCameraTrl = new Translation3d(0.1, 0, 0.5);
+      // and pitched 15 degrees up.
+      Rotation3d robotToCameraRot = new Rotation3d(0, Math.toRadians(-15), 0);
+      Transform3d robotToCamera = new Transform3d(robotToCameraTrl, robotToCameraRot);
+
+      // Add this camera to the vision system simulation with the given
+      // robot-to-camera transform.
+      // visionSim.addCamera(camSim, robotToCamera);
+
     }
   }
 
