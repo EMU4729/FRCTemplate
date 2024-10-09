546ec75bd3ac3b0f437b7687762ca8472d247cae
Neel Sharma <nsdj.sharma@gmail.com>
2024-10-09 20:09:00 +1100

    fix typo

--- src/main/java/frc/robot/auto/TestAuto.java
+++ src/main/java/frc/robot/auto/TestAuto.java
@@ -4,14 +4,14 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Subsystems;
-import frc.robot.commands.DriveToPoint;
+import frc.robot.commands.SwerveDriveToPoint;
 
 public class TestAuto extends SequentialCommandGroup {
   public TestAuto() {
     addCommands(
         Subsystems.swerveDrive
             .runOnce(() -> Subsystems.swerveDrive.resetOdometry(new Pose2d(1.4, 5.5, Rotation2d.fromDegrees(0)))),
-        new DriveToPoint(new Pose2d(1, 1, Rotation2d.fromDegrees(90))));
+        new SwerveDriveToPoint(new Pose2d(1, 1, Rotation2d.fromDegrees(90))));
   }
 
 }
