e3db52f9d75ddb45e0d5a9b58380ad46688005d3
roterrex <joel.roterex@gmail.com>
2024-12-18 00:50:21 +1100

    added comments and axis inversion

--- src/main/java/frc/robot/utils/RangeMath/DriveBaseFit.java
+++ src/main/java/frc/robot/utils/RangeMath/DriveBaseFit.java
@@ -6,6 +6,10 @@ public class DriveBaseFit {
 
   protected AxesFit axesLin;
   protected AxesFit axesYaw;
+
+  private boolean invertX = false;
+  private boolean invertY = false;
+  private boolean invertYaw = false;
   
   protected double ModifierTurnRateBySpeed;
 
@@ -19,16 +23,56 @@ public class DriveBaseFit {
     this.ModifierTurnRateBySpeed = ModifierTurnRateBySpeed;
   }
 
+  /**
+   * ---Decorator for the main constructor---<p>
+   * invert the input x axis
+   * */
+  public DriveBaseFit invertX()  {invertX = true;   return this;}
+  /**
+   * ---Decorator for the main constructor---<p>
+   * inverts the input y axis [swerve only]
+   * */
+  public DriveBaseFit invertY()  {invertY = true;   return this;}
+  /**
+   * ---Decorator for the main constructor---<p>
+   * inverts the yaw axis
+   * */
+  public DriveBaseFit invertYaw(){invertYaw = true; return this;}
+  /** apply the specified curve to the provided input and returns (no boost, no limit) */
   public double[] fitTank(double x, double yaw){ return fitTank(x, yaw, 0, 0);}
+  /** apply the specified curve to the provided input and returns<p>
+  * 
+  * Booster - This input axis is a percent linear interpolation between normal max and boosted
+  * max (0 = norm, 1=full boost)<p>
+  * 
+  * Limiter - The second input axis is a percent linear interpolation between boosted max and
+  * limited max (0 = norm, 1=full limit)<p>
+   */
   public double[] fitTank(double x, double yaw, double boostPercent, double limitPercent){
+    if(invertX)  {x = -x;}
+    if(invertYaw){yaw = -yaw;}
+
     x = axesLin.fit(x, boostPercent, limitPercent);
     yaw = axesYaw.fit(yaw, boostPercent, limitPercent);
     yaw *= ((1-ModifierTurnRateBySpeed) + (ModifierTurnRateBySpeed*(Math.abs(x)/axesLin.outAbsMax)));
     return new double[]{x, 0, yaw};
   }
 
+  /** apply the specified curve to the provided input and returns (no boost, no limit) */
   public double[] fitSwerve(double x, double y, double yaw){ return fitSwerve(x, y, yaw, 0, 0);}
+  /** apply the specified curve to the provided input and returns<p>
+  * 
+  * Booster - This input axis is a percent linear interpolation between normal max and boosted
+  * max (0 = norm, 1=full boost)<p>
+  * 
+  * Limiter - The second input axis is a percent linear interpolation between boosted max and
+  * limited max (0 = norm, 1=full limit)<p>
+   */
   public double[] fitSwerve(double x, double y, double yaw, double boostPercent, double limitPercent){
+    if(invertX)  {x = -x;}
+    if(invertY)  {y = -y;}
+    if(invertYaw){yaw = -yaw;}
+
     double angle = Math.atan2(y, x);
     double power = Math.hypot(x, y);
 
