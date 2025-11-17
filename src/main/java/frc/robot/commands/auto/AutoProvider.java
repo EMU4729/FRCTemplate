b31e98ae7fec241f957e2470790d912b2aabb372
Neel Sharma <nsdj.sharma@gmail.com>
2025-11-17 20:10:47 +1100

    remove unused import

--- src/main/java/frc/robot/commands/auto/AutoProvider.java
+++ src/main/java/frc/robot/commands/auto/AutoProvider.java
@@ -6,7 +6,6 @@ import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
